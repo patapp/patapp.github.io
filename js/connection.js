2af247e8d0522c7f7afb672e9ea3f2d1c2ecfde9
Samuli Virtanen <samuli.virtanen@tucom.fi>
2018-11-20 14:35:43 +0200

    updates/dev

--- js/connection.js
+++ js/connection.js
@@ -3,15 +3,36 @@
 let sessionExists = 0;
 let sessionID = '';
 let sessionToken = '';
+let sessionPermissions = []
 
-const getJSON = async ( rMethod , request, rParams='', sendSessionInfo=1 ) => {
+const serialize = (obj) => {
+  var str = [];
+  for (var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
+
+const getJSON = async ( rMethod , request, rParams='', dataObject = {}, sendSessionInfo=1 ) => {
 
   // REMOVE THIS IN PRODUCTION VERSION
+  console.log('getJSON' + rMethod + ':' + request);
   console.log(API_URL + request + rParams + ( sendSessionInfo && sessionExists ? ( rParams == '' ? '?' : '&' ) + 'session_id=' + sessionID + '&session_token=' + sessionToken : '' ));
 
+  console.log('fetch data obj:');
+  console.log(dataObject);
+
   const response = await fetch( API_URL + request + rParams + ( sendSessionInfo && sessionExists ? ( rParams == '' ? '?' : '&' ) + 'session_id=' + sessionID + '&session_token=' + sessionToken : '' ), {
-    method: "POST"
+    method: "POST",
+    body: serialize(dataObject),
+    headers:
+    {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
   } );
+  
+  console.log('testing');
   
   const json = await response.json();
   console.log(json);
@@ -51,13 +72,12 @@ const cookieToken = getCookie ( 'pat_session_token' );
 
 const sessionCheck = async () => {
   let response = '';
-  const j = await getJSON('POST', 'session/check', '?session_id=' + cookieID + '&session_token=' + cookieToken, 0).then( (r) => {
+  // We need to pass the session id + token manually, because at this point session does not exists yet.
+  const j = await getJSON('POST', 'session/check', '', {session_id : cookieID, session_token : cookieToken}, 0).then( (r) => {
     if ( r.success == 1 && r.session_exists == 1) {
       sessionExists = 1;
       sessionID = cookieID;
       sessionToken = cookieToken;
-      //setCookie ( 'pat_session_id', cookieID, 60 );
-      //setCookie ( 'pat_session_token', cookieToken, 60 );
       }
     response = r;
     });
@@ -66,10 +86,11 @@ const sessionCheck = async () => {
 
 
 if ( typeof cookieID != 'undefined' && typeof cookieToken != 'undefined' && cookieID != "" && cookieToken != "" ) {
-console.log('Session Check!' + cookieID);
+console.log('*** SessionCheck!' + cookieID);
 sessionCheck().then( (r) => {
   if ( r.success ) {
-    console.log ( r.session_exists );
+    //console.log ( r.session_exists );
+    sessionPermissions = r.permissions;
     }
 });
   
--- js/global.js
+++ js/global.js
@@ -1,2 +1,2 @@
-//const API_URL = 'http://127.0.0.1:3114/';   // local
-const API_URL = 'http://82.181.20.24:3114/';   // public-server
+const API_URL = 'http://127.0.0.1:3114/';   // local
+//const API_URL = 'http://82.181.20.24:3114/';   // public-server
