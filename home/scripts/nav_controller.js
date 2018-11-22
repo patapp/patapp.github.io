69894b80ee3162080beca16f7e80910202ef1f71
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-11-22 21:27:44 +0200

    Add: base tab switching functionality

--- home/scripts/nav_controller.js
+++ home/scripts/nav_controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const menu = document.getElementById('menu');
 const bottomNav = document.querySelector('.bottom-nav-bar');
 const radioButtons = document.querySelectorAll('input[type="radio"] ~ label');
@@ -8,11 +9,57 @@ const bottomNavHeight = bottomNav.offsetHeight;
 const navBarHeight = 43;
 const navBarHiddenPos = (bottomNavHeight - navBarHeight) * -1;
 
+const tabElements = document.querySelectorAll('.tab');
+
+const transitionSpeed = 130;
+
+let currentTab = 0;
+const HOME    = 0,
+      SEARCH  = 1,
+      PROFILE = 2;
+
+const hideTabWithTransition = (tab) => {
+  const targetTab = tabElements[tab];
+  targetTab.classList.toggle('visible');
+  setTimeout(() => {
+    targetTab.classList.toggle('show-element');
+  }, transitionSpeed);
+}
+const showTabWithTransition = (tab) => {
+  const targetTab = tabElements[tab];
+  targetTab.classList.toggle('show-element');
+  setTimeout(() => {
+    targetTab.classList.toggle('visible');
+  }, transitionSpeed);
+}
+
+const changeToTab = (tab) => {
+  hideTabWithTransition(currentTab);
+  currentTab = tab;
+  setTimeout(() => {
+    showTabWithTransition(currentTab);
+  }, transitionSpeed);
+}
+
+document.getElementById('home-btn')
+.addEventListener('change', ()=> {
+  changeToTab(HOME);
+});
+
+document.getElementById('search-btn')
+.addEventListener('change', ()=> {
+  changeToTab(SEARCH);
+});
+
+document.getElementById('profile-btn')
+.addEventListener('change', ()=> {
+  changeToTab(PROFILE);
+});
+
 const styleHidden = document.createElement('style');
 styleHidden.type = 'text/css';
 styleHidden.innerHTML = `.menu-hidden { bottom: ${navBarHiddenPos}px }`;
 document.getElementsByTagName('head')[0].appendChild(styleHidden);
-
 bottomNav.classList.add('menu-hidden');
 
 menu.addEventListener('change', ()=> {
@@ -21,9 +68,8 @@ menu.addEventListener('change', ()=> {
   radioButtons.forEach(element => {
     element.classList.toggle('btn-hidden');
   });
-
 });
 
 setTimeout(() => {
-  bottomNav.style.transition = 'bottom 300ms cubic-bezier(0.5, -0.4, 0.5, 1.4) 0s';
-}, 10);
+  bottomNav.style.transition = 'bottom 300ms cubic-bezier(0.4, 0, 0.2, 1) 0s';
+}, 1);
