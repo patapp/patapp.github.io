2840db7c4504530b3df6b74642ae81fb4f847ee5
Samuli Virtanen <samuli.virtanen@tucom.fi>
2018-11-26 23:17:24 +0200

    new backend server URL

--- src/js/global.js
+++ src/js/global.js
@@ -9,7 +9,7 @@
   +--------------------------+------------------------- */
 
 //const API_URL = 'http://127.0.0.1:3114/';   // local
-const API_URL = 'http://82.181.20.24:3114/';   // public-server
+const API_URL = 'https://tucloud.fi/pat/';   // public-server
 
 const BASE_ADDR = '/pat-project-frontend/'; // if in root folder, set this to '/'
 
