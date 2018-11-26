3ece55e7395dbedc90765b195be3c510d4885bc2
Jamie Yoon <rkawk3316@gmail.com>
2018-11-26 16:10:25 +0200

    Code cleanup

---
+++ src/js/trending-feed-controller.js
@@ -0,0 +1,26 @@
+const postElement = `
+<li class="post" id="{id}">
+            
+    <div class="post-header">
+        <div class="post-header__picture"></div>
+        <div class="post-header__username"></div>
+    </div>
+
+    <div class="post-media"></div>
+
+    <!-- TODO: add rating graphic -->
+
+    <div class="post-info">
+        <div class="post-info__tags"></div>
+        <div class="post-info__description"></div>
+        <div class="post-info__comments"></div>
+                
+        <div class="post-info__timestamp">
+            <p></p><span></span>
+        </div>
+
+    </div>
+
+</li>
+`;
+
