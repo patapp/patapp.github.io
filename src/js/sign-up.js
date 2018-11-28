0490dc04cada4a8c54528220d99f3c1eb27c16f0
Samuli Virtanen <samuli.virtanen@tucom.fi>
2018-11-28 14:59:55 +0200

    added function checkIsUsernameAvailable() for FrontEnd

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -7,4 +7,21 @@
   |   `-'   `-'  `-' `-'     |  "Jamie" GeonHui Yoon     |
   |   - a place for pets -   |  Samuli Virtanen          |
   +--------------------------+--------------------------*/
-  
+ 
+const checkIsUsernameAvailable = (u) => {
+  if ( u != "" ) {
+    getJSON('GET', 'users/username-available/'+u, '', {}, 0).then( (r) => {
+      if ( r.success ) {
+          if ( r.available ) {
+            console.log('OK: username is ok and available - replace this with code!');
+            }else{
+            console.log('ERROR: username is ok, but not available - replace this with code!');
+            }
+        }else{
+          console.log('ERROR: ' + r.error ); //  - replace this with code!
+        }
+      });
+    }else{
+      console.log('ERROR: username cannot be empty!'); //  - replace this with code!
+    }
+  };
