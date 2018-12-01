1314dff6e9af511b8747cc15f2dc9e67dc20fae6
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-12-01 23:24:32 +0200

    Change: autoredirect -> on

--- src/js/global.js
+++ src/js/global.js
@@ -24,7 +24,7 @@ const REDIRECT_DELAY = false;
 
 /* Redirect automatically from wrong page to right page if SESSION state is wrong. 
 Plase note: Does not make effect if DEBUG_MODE = false */
-const AUTO_REDIRECT = false;
+const AUTO_REDIRECT = true;
 
 let sessionExists = false;
 let sessionID = '';
