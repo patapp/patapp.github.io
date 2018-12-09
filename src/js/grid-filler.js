819aab66478338f3e60170e4c933125b1caaa8e8
Samuli Virtanen <samuli.virtanen@tucom.fi>
2018-12-09 15:49:16 +0200

    gridviewer added and integrated

---
+++ src/js/grid-filler.js
@@ -0,0 +1,155 @@
+"use strict";
+
+  /* ------------------------+-------------------------------+
+  |   .----.  .--.  .---.    |  CREATED BY TEAM JJS          |
+  |   | {}  }/ {} \{_   _}   +-------------------------------+
+  |   | .--'/  /\  \ | |     |  Joonas Kauppinen             |
+  |   `-'   `-'  `-' `-'     |  "Jamie" GeonHui Yoon         |
+  |   - a place for pets -   |  Samuli Virtanen              |
+  +--------------------------+-------------------------------+
+  | https://github.com/joonasmkauppinen/pat-project-backend  |
+  | https://github.com/joonasmkauppinen/pat-project-frontend |
+  +-------------------------------------------------------- */
+
+class gridFiller {
+  
+  constructor(elementID, searchType, loadItemsCount=27, searchTerm='') {
+    this.elementID = elementID;
+    this.searchType = searchType;
+    this.loadItemsCount = loadItemsCount;
+    this.searchTerm = searchTerm;
+    this.obj = document.getElementById(elementID);
+    console.log('[gridFiller] New Gridfiller initialized. elementID: ' + elementID + ' searchType: ' + searchType + ' loadItemsCount: ' + loadItemsCount);
+  }
+  
+  init(){
+    this.reset();
+  }
+  
+  setSearchTerm(newTerm) {
+    this.searchTerm = newTerm;
+    this.reset();
+  }
+  setSearchType(newType) {
+    this.searchType = newType;
+    this.reset();
+  }
+  setSearchTypeAndTerm(newType,newTerm) {
+    this.searchTerm = newTerm;
+    this.searchType = newType;
+    this.reset();
+  }
+  
+  reset(){
+    this.postsInitialized = 0;
+    this.postsDataLoaded = 0;
+    this.postsDataContentLoadedTo = 0;
+    this.postsDataArray = [];
+    this.postsDataCount = -1;
+    this.obj.innerHTML = '';
+    console.log('[gridFiller] Reset.');
+    this.append();
+    this.loadPosts();
+  }
+  
+  append(amount) {
+    if ( !amount ) amount = this.loadItemsCount;
+    conLog ( '[gridFiller] append() : Called with amount: ' + amount );
+    let maxAmount = amount;
+    if ( this.postsDataLoaded ) {
+      maxAmount = (this.postsDataCount - this.postsInitialized);
+      conLog('[gridFiller] append() : Elements Left: ' + maxAmount);
+      if ( maxAmount < amount ) conLog ( '[gridFiller] Only ' + maxAmount + ' items left, i will be rendering only just ' + maxAmount );
+      if ( maxAmount == 0 ) {
+        conLog('[gridFiller] append() : No items left! Cannot append more posts!');
+        return;
+      }
+    }
+    let addedAmount = 0;
+    for (let i=0; i<amount && i<maxAmount; i++) {
+      const newElement = document.createElement('LI');
+      newElement.id = this.elementID + '-item-' + (i+this.postsInitialized);
+      this.obj.appendChild(newElement);
+      addedAmount++;
+    }
+    this.postsInitialized=this.postsInitialized+addedAmount;
+    conLog('[gridFiller] New elements are created!');
+    if ( this.postsDataLoaded ) this.fetchPosts();
+  }
+
+  renderPosts (from, to) {
+    
+    conLog('[gridFiller] renderPosts() : Rendering: ' + from + ' --> ' + to);  
+    let renderItems = [];
+    
+    for (let p=from; p<=to; p++) {
+      renderItems.push(this.postsDataArray[p]);
+    }
+    
+    getJSON('POST','posts/getcontent', '', {items: renderItems.join('-')} ).then( (res) => {
+      console.log('THIS IS WHAT I GOT: ');
+      console.log(res);
+      for (let p=from; p<=to; p++) {
+        const liElem = document.getElementById(this.elementID + '-item-' + p);
+        const newImg = document.createElement('IMG');
+        liElem.style.backgroundColor = `#${res.post_data[this.postsDataArray[p]].color}`;
+        liElem.style.backgroundImage = `url('${API_URL + res.post_data[this.postsDataArray[p]].thumbnail}')`;
+        liElem.style.backgroundSize = 'cover';
+        liElem.style.backgroundRepeat = 'none';
+        liElem.style.backgroundPosition = 'center';
+        liElem.className = 'clickable';
+        liElem.innerHTML = ' ';
+        liElem.addEventListener('click', () => {
+          gridItemClicked ( this.elementID, this.postsDataArray[p] );
+        });
+        //newImg.src = console.log(this.elementID + '-item-' + p + ' value: ' + this.postsDataArray[p]);
+        //newImg.src = API_URL + res.post_data[this.postsDataArray[p]].thumbnail;
+        //newImg.height = 100;
+        //newImg.width = 100 ;
+        //liElem.appendChild(newImg);
+      }
+    });
+  }
+  
+   destroyUnneeded() {
+   console.log('DD'+this.postsInitialized);
+    if ( this.postsInitialized > this.postsDataCount ) {
+      for ( let i = this.postsDataCount; i<this.postsInitialized; i++ ){
+        const destroyObject = document.getElementById(this.elementID + '-item-' + i);
+        if ( typeof destroyObject != 'undefined' ) {
+          destroyObject.remove();
+          }
+        }
+      }
+   }
+  
+   fetchPosts () {
+    conLog('[gridFiller] fetchPosts() : Skeletons: ' + this.postsInitialized + ", TotalItems: " + this.postsDataCount + ', ContentLoadedTo: ' + this.postsDataContentLoadedTo );
+    if ( this.postsDataContentLoadedTo < this.postsDataCount ) {
+      const renderFrom = this.postsDataContentLoadedTo;
+      const renderTo   = (this.postsInitialized < this.postsDataCount ? this.postsInitialized : this.postsDataCount) - 1;
+      this.postsDataContentLoadedTo = renderTo + 1;
+      conLog('[gridFiller] I will render from ' + renderFrom + ' to ' + renderTo );
+      this.renderPosts (renderFrom, renderTo);
+    }else{
+      conLog('[gridFiller] fetchPosts() : all already fetched');
+    }
+  }
+
+  loadPosts() {
+    conLog('[gridFiller] loadPosts() : Called.');
+    getJSON('POST', 'posts', '', {filter_by: this.searchType, filter_string: this.searchTerm} ).then((res) => {
+      conLog('[gridFiller] loadPosts() : Total number of posts: ' + res.posts_count);
+      this.postsDataArray = res.posts;
+      console.log('OBJ ARRAY DUMP');
+      console.log(this.postsDataArray);
+      this.postsDataLoaded = 1;
+      this.postsDataCount = res.posts_count;
+      this.destroyUnneeded();
+      this.fetchPosts();
+    });
+  }
+
+  
+
+}
--- src/js/home-first.js
+++ src/js/home-first.js
@@ -13,6 +13,51 @@
 
 const loadingScreen = document.querySelector('.loading-screen');
 
+const gridSearch = new gridFiller('grid-search', 'landing', 27 );
+const gridUserPosts = new gridFiller('grid-userposts', 'user', 18 );
+
+
+let lastSearchTerm = '';
+let timeoutForSearch;
+
+  const gridSearchListView = document.getElementById('grid-search');
+  const tagsListView = document.getElementById('list-search-tags');
+  const usersListView = document.getElementById('list-search-users');
+
+
+  const gridItemClicked = ( grid, itemID ) => {
+    alert( 'GridItemClicked! Grid: ' + grid + ' ItemID: ' + itemID );
+    }
+
+  const setSearchType = (searchType) => {
+    const searchOptions = document.querySelectorAll('#search-for-options li');
+      searchOptions.forEach((obj) => {
+        obj.classList.remove('selected');
+      });
+    const selectedValue = document.getElementById(searchType);
+    selectedValue.classList.add('selected');
+    
+    switch ( searchType ) {
+      case 'search-for-item-post':
+        tagsListView.classList.add('hide');
+        usersListView.classList.add('hide');
+        gridSearchListView.classList.remove('hide');
+        break;
+      case 'search-for-item-tag':
+        gridSearchListView.classList.add('hide');
+        usersListView.classList.add('hide');
+        tagsListView.classList.remove('hide');
+        break;
+      case 'search-for-item-user':
+        gridSearchListView.classList.add('hide');
+        tagsListView.classList.add('hide');
+        usersListView.classList.remove('hide');
+        break;
+      }
+  };
+
+
+
 /* This function is called only, if the session exists and user is logged in. */
 const appIsReady = () => {
 
@@ -21,6 +66,115 @@ const appIsReady = () => {
   loadingScreen.classList.add('hidden');
 
   loadPosts(); // Load TRENDING PAGE posts / post content
+
+  gridUserPosts.setSearchTerm(sessionLoggedInUserID);
+
+
+
+  gridSearch.init();
+  gridUserPosts.init();
+
+const tagItemClicked = ( tag ) => {
+  const searchBar = document.getElementById('search-bar');
+  searchBar.value = '=' + tag;
+  setSearchType('search-for-item-post');
+  doSearch();
+  }
+
+const userItemClicked = ( userID, userName ) => {
+  alert ( 'User Item clicked! User ID: ' + userID + ', Name: ' + userName );
+  }  
+  
+  const searchTab = document.getElementById('search-tab');
+  searchTab.addEventListener('scroll', () => {
+      if (isVisibleOnScreen(searchLoadTrigger)) {
+          gridSearch.append();
+      }
+  });
+  
+  const doSearch = () => {
+    const searchBar = document.getElementById('search-bar');
+    if ( searchBar.value != lastSearchTerm ) {
+
+      const rcPost = document.getElementById('result-count-post');
+      const rcTag  = document.getElementById('result-count-tag');
+      const rcUser = document.getElementById('result-count-user');
+    
+      rcPost.textContent = '';
+      rcTag.textContent = '';
+      rcUser.textContent = '';
+    
+      lastSearchTerm  = searchBar.value;
+      let searchValue = searchBar.value;
+      tagsListView.innerHTML = '';
+      usersListView.innerHTML = '';
+      
+      if ( searchValue.substring(0,1) == '=' ) {
+        searchValue = searchValue.substring(1);
+        gridSearch.setSearchTypeAndTerm('Tag', searchValue);
+        }else{
+        gridSearch.setSearchTypeAndTerm('TagSearch', searchValue);
+        }
+      
+      
+      getJSON('POST','users', '', {search_term: searchValue} ).then( (res) => {
+        if ( res.success ) {
+        usersListView.innerHTML = '';
+        console.log(res);
+          if ( res.user_count > 0 ) {
+            rcUser.textContent = res.user_count;
+            for( let i=0; i<res.user_count; i++ ){
+              const newLI = document.createElement('LI');
+              newLI.textContent = res.users[i].name;
+              newLI.className = 'clickable';
+              usersListView.appendChild(newLI);
+              newLI.addEventListener('click', () => {
+                userItemClicked(res.users[i].id, res.users[i].name);
+                });
+              }
+            }
+          }
+      });
+      getJSON('POST','tags', '', {search_term: searchValue} ).then( (res) => {
+        if ( res.success ) {
+        tagsListView.innerHTML = '';
+        console.log(res);
+          if ( res.tag_count > 0 ) {
+            rcTag.textContent = res.tag_count;
+            for( let i=0; i<res.tag_count; i++ ){
+              const newLI = document.createElement('LI');
+              newLI.className = 'clickable';
+              newLI.textContent = res.tags[i];
+              tagsListView.appendChild(newLI);
+              newLI.addEventListener('click', () => {
+                tagItemClicked(res.tags[i]);
+                });
+              }
+            }
+          }
+      });
+      
+      }
+    
+    
+  };
+  const searchChanged = () => {
+    clearTimeout(timeoutForSearch);
+    timeoutForSearch = setTimeout(doSearch, 250);
+  };
+  const searchBar = document.getElementById('search-bar');
+  searchBar.addEventListener('change', () => { searchChanged(); });
+  searchBar.addEventListener('keypress', () => { searchChanged(); });
+  searchBar.addEventListener('keyup', () => { searchChanged(); });
+  
+
+
+  const searchOptions = document.querySelectorAll('#search-for-options li');
+  searchOptions.forEach((obj) => {
+    obj.addEventListener('click', (e) => {
+      setSearchType(obj.id);
+    });
+  });
 
   /* Modify app here to meet the permissions
     
