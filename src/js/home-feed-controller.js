780610e75b06e14b2024143af8cf0f7c36368241
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-12-10 23:18:21 +0200

    Add: logging for username click

--- src/js/home-feed-controller.js
+++ src/js/home-feed-controller.js
@@ -120,9 +120,12 @@ const getUsernameFromDocument = (id) => {
 
 const checkTarget = (id, target) => {
 
+  console.log('[checkTarget] called');
+
   switch (target.tagName) {
     
     case "IMG":
+      console.log('case IMG');
       if (target.closest('div').className === "post-header__picture") {
         getUsernameFromDocument(id);
         toggleVisitingProfile();
@@ -130,6 +133,7 @@ const checkTarget = (id, target) => {
     break;
     
     case "P":
+      console.log('case P');
       if (target.closest('div').className === "post-header__username") {
         getUsernameFromDocument(id);
         toggleVisitingProfile();
