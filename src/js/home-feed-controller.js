0a11b21c7ea68eaf72f0ab042e8ca9f31611c18e
Jamie Yoon <rkawk3316@gmail.com>
2018-12-11 05:56:07 +0200

    Fixed front page content loading issue

--- src/js/home-feed-controller.js
+++ src/js/home-feed-controller.js
@@ -113,21 +113,26 @@ const updatePostRating = (post, value) => {
 
 
 const btn = document.getElementById('visiting-profile_follow-btn');
-btn.addEventListener('click', () => {
-const btntxt = document.querySelector('#visiting-profile_follow-btn .follow-btn-text span');
-if ( btntxt.textContent == 'FOLLOW' || btntxt.textContent == 'UNFOLLOW' ) {
-  const toValue = (btntxt.textContent == 'UNFOLLOW' ? false : true );
-  console.log(toValue);
-  btntxt.textContent = ' ... ';
-  setFollowUser(visitingUserID, toValue).then( (r) => {
-    if ( r == false ) {
-      btntxt.textContent = 'FOLLOW';
-      }else{
-      btntxt.textContent = 'UNFOLLOW';
-      }
-  });
+try {
+  btn.addEventListener('click', () => {
+    const btntxt = document.querySelector('#visiting-profile_follow-btn .follow-btn-text span');
+    if ( btntxt.textContent == 'FOLLOW' || btntxt.textContent == 'UNFOLLOW' ) {
+      const toValue = (btntxt.textContent == 'UNFOLLOW' ? false : true );
+      console.log(toValue);
+      btntxt.textContent = ' ... ';
+      setFollowUser(visitingUserID, toValue).then( (r) => {
+        if ( r == false ) {
+          btntxt.textContent = 'FOLLOW';
+          }else{
+          btntxt.textContent = 'UNFOLLOW';
+          }
+      });
+    }
+    });
 }
-});
+catch (err){
+  console.log(err);
+}
 
 
 
