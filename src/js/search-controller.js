1df77dc1ae07cc4353249f647fccb72a22904538
Jamie Yoon <rkawk3316@gmail.com>
2018-12-11 02:59:52 +0200

    Added reset button for search bar

--- src/js/search-controller.js
+++ src/js/search-controller.js
@@ -4,13 +4,17 @@ const searchTab = document.getElementById('search-tab');
 const feedGrid = document.querySelector('.search-tab__feed > ul');
 const searchLoadTrigger = document.getElementById('search-load-trigger');
 const searchBar = document.getElementById('search-bar');
+const searchReset = document.getElementById('reset');
+
+searchReset.addEventListener('click', () => {
+    searchBar.value='';
+});
+
 
 const makeSearch = (value) => {
     changeToTab(SEARCH);
     searchBar.value = value;
 }
-
-
 
 //appendItems();
 
